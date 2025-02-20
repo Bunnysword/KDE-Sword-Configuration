96135326ad8c02073681da7fd38592b30b5e1850
Bunnysword <129748119+Bunnysword@users.noreply.github.com>
2025-02-20 17:22:48 +0300

    Update kdeswordtheme.sh

--- kdeswordtheme.sh
+++ kdeswordtheme.sh
@@ -1,4 +1,4 @@
-mkdir ~/.local/share/color-schemes
+#mkdir ~/.local/share/color-schemes
 mv SwordScheme.colors ~/.local/share/color-schemes/
 mv SwordProfile.profile ~/.local/share/konsole/
 mv kxkbrc ~/.config
