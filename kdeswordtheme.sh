5159e460c517f8ffc6a323d2dec5c5707fcb3d8f
Bunnysword <129748119+Bunnysword@users.noreply.github.com>
2025-02-20 17:55:04 +0300

    Update kdeswordtheme.sh

--- kdeswordtheme.sh
+++ kdeswordtheme.sh
@@ -2,5 +2,7 @@ mkdir ~/.local/share/color-schemes
 mv SwordScheme.colors ~/.local/share/color-schemes/
 mv SwordProfile.profile ~/.local/share/konsole/
 mv kxkbrc ~/.config
+mv kcm-about-distrorc /home/bsword/.config/kdedefaults/
+
 #применение цветовой схемы
 plasma-apply-colorscheme SwordScheme
