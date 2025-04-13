30206ff071d78c357cae2c76e8b12deb1e6b69cf
Bunnysword <129748119+Bunnysword@users.noreply.github.com>
2025-04-13 20:48:45 +0300

    Update kdeswordtheme.sh

--- kdeswordtheme.sh
+++ kdeswordtheme.sh
@@ -2,7 +2,6 @@ mkdir ~/.local/share/color-schemes
 mv SwordScheme.colors ~/.local/share/color-schemes/
 mv swordprofile.profile ~/.local/share/konsole/
 mv WhiteOnBlack.colorscheme ~/.local/share/konsole/
-mv kxkbrc ~/.config
 mv kcm-about-distrorc ~/.config/kdedefaults/
 wget https://raw.githubusercontent.com/Bunnysword/Confiles/refs/heads/main/conf/kde/bugs.png && mv bugs.png ~/.config/kdedefaults/
 
